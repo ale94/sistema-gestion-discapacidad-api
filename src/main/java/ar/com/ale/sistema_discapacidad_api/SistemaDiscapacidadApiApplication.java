b4c9cc793ad46e9f185d80422d5b65b9dab0e604
ale94 <alejandrorua111@gmail.com>
2026-03-05 16:41:05 -0300

    feat: add configuration files and initial structure of the spring boot project

---
+++ src/main/java/ar/com/ale/sistema_discapacidad_api/SistemaDiscapacidadApiApplication.java
@@ -0,0 +1,13 @@
+package ar.com.ale.sistema_discapacidad_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaDiscapacidadApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaDiscapacidadApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ar/com/ale/sistema_discapacidad_api/SistemaDiscapacidadApiApplicationTests.java
@@ -0,0 +1,13 @@
+package ar.com.ale.sistema_discapacidad_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaDiscapacidadApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
