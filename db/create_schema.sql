9e2c5cc2cbc91a77ff18786f996796bf66a05740
ale94 <alejandrorua111@gmail.com>
2026-04-05 23:57:00 -0300

    refactor: refactor data.sql and schema.sql

--- db/create_schema.sql
+++ db/create_schema.sql
@@ -1,151 +1,177 @@
 -- =========================
 -- PERSON
 -- =========================
-CREATE TABLE person (
-    id BIGSERIAL PRIMARY KEY,
-    first_name VARCHAR(100),
-    last_name VARCHAR(100),
-    dni BIGSERIAL UNIQUE,
-    civil_status VARCHAR(50),
-    date_birth DATE,
-    tutor VARCHAR(100),
-    phone BIGSERIAL,
-    gender VARCHAR(100),
+CREATE TABLE person
+(
+    id                BIGSERIAL PRIMARY KEY,
+    first_name        VARCHAR(100),
+    last_name         VARCHAR(100),
+    dni               BIGINT UNIQUE,
+    civil_status      VARCHAR(50),
+    date_birth        DATE,
+    tutor             VARCHAR(100),
+    phone             BIGSERIAL,
+    gender            VARCHAR(100),
     registration_date DATE
 );
 -- =========================
 -- EDUCATION
 -- =========================
-CREATE TABLE education (
-    id BIGSERIAL PRIMARY KEY,
-    name VARCHAR(200),
-    address VARCHAR(200),
+CREATE TABLE education
+(
+    id              BIGSERIAL PRIMARY KEY,
+    name            VARCHAR(200),
+    address         VARCHAR(200),
     education_level VARCHAR(100),
-    person_id BIGINT UNIQUE,
-    CONSTRAINT fk_education_person FOREIGN KEY (person_id) REFERENCES person(id) ON DELETE CASCADE
+    person_id       BIGINT UNIQUE,
+    CONSTRAINT fk_education_person FOREIGN KEY (person_id) REFERENCES person (id) ON DELETE CASCADE
 );
 -- =========================
 -- WORK
 -- =========================
-CREATE TABLE work (
-    id BIGSERIAL PRIMARY KEY,
-    company_name VARCHAR(200),
-    status VARCHAR(200),
-    address VARCHAR(200),
-    social_work BOOLEAN,
+CREATE TABLE work
+(
+    id               BIGSERIAL PRIMARY KEY,
+    company_name     VARCHAR(200),
+    status           VARCHAR(200),
+    address          VARCHAR(200),
+    social_work      BOOLEAN,
     name_social_work VARCHAR(200),
-    person_id BIGINT UNIQUE,
-    CONSTRAINT fk_work_person FOREIGN KEY (person_id) REFERENCES person(id) ON DELETE CASCADE
+    person_id        BIGINT UNIQUE,
+    CONSTRAINT fk_work_person FOREIGN KEY (person_id) REFERENCES person (id) ON DELETE CASCADE
 );
 -- =========================
 -- HEALTH
 -- =========================
-CREATE TABLE health (
-    id BIGSERIAL PRIMARY KEY,
-    cud_number VARCHAR(50),
-    active_cud BOOLEAN,
+CREATE TABLE health
+(
+    id                       BIGSERIAL PRIMARY KEY,
+    cud_number               VARCHAR(50),
+    active_cud               BOOLEAN,
     rehabilitation_treatment BOOLEAN,
-    diagnostic VARCHAR(50),
-    disability_type VARCHAR(50),
-    person_id BIGINT UNIQUE,
-    CONSTRAINT fk_health_person FOREIGN KEY (person_id) REFERENCES person(id) ON DELETE CASCADE
+    diagnostic               VARCHAR(50),
+    disability_type          VARCHAR(50),
+    person_id                BIGINT UNIQUE,
+    CONSTRAINT fk_health_person FOREIGN KEY (person_id) REFERENCES person (id) ON DELETE CASCADE
 );
 -- =========================
 -- ADDRESS
 -- =========================
-CREATE TABLE address (
-    id BIGSERIAL PRIMARY KEY,
-    street VARCHAR(200),
-    district VARCHAR(100),
-    locality VARCHAR(100),
-    province VARCHAR(100),
+CREATE TABLE address
+(
+    id        BIGSERIAL PRIMARY KEY,
+    street    VARCHAR(200),
+    district  VARCHAR(100),
+    locality  VARCHAR(100),
+    province  VARCHAR(100),
     person_id BIGINT UNIQUE,
-    CONSTRAINT fk_address_person FOREIGN KEY (person_id) REFERENCES person(id) ON DELETE CASCADE
+    CONSTRAINT fk_address_person FOREIGN KEY (person_id) REFERENCES person (id) ON DELETE CASCADE
 );
 -- =========================
 -- BENEFIT
 -- =========================
-CREATE TABLE benefit (
-    id BIGSERIAL PRIMARY KEY,
-    pension BOOLEAN,
-    free_pass BOOLEAN,
+CREATE TABLE benefit
+(
+    id              BIGSERIAL PRIMARY KEY,
+    pension         BOOLEAN,
+    free_pass       BOOLEAN,
     federal_program BOOLEAN,
-    auh BOOLEAN,
-    merchandise BOOLEAN,
-    person_id BIGINT UNIQUE,
-    CONSTRAINT fk_benefit_person FOREIGN KEY (person_id) REFERENCES person(id) ON DELETE CASCADE
+    auh             BOOLEAN,
+    merchandise     BOOLEAN,
+    person_id       BIGINT UNIQUE,
+    CONSTRAINT fk_benefit_person FOREIGN KEY (person_id) REFERENCES person (id) ON DELETE CASCADE
 );
 -- =========================
 -- FAMILY MEMBER
 -- =========================
-CREATE TABLE family_member (
-    id BIGSERIAL PRIMARY KEY,
-    first_name VARCHAR(100),
-    last_name VARCHAR(100),
-    dni BIGSERIAL,
-    age INTEGER,
+CREATE TABLE family_member
+(
+    id           BIGSERIAL PRIMARY KEY,
+    first_name   VARCHAR(100),
+    last_name    VARCHAR(100),
+    dni          BIGINT,
+    age          INTEGER,
     civil_status VARCHAR(100),
-    parentage VARCHAR(100),
-    occupation VARCHAR(100),
-    person_id BIGINT,
-    CONSTRAINT fk_family_member_person FOREIGN KEY (person_id) REFERENCES person(id) ON DELETE CASCADE
+    parentage    VARCHAR(100),
+    occupation   VARCHAR(100),
+    person_id    BIGINT,
+    CONSTRAINT fk_family_member_person FOREIGN KEY (person_id) REFERENCES person (id) ON DELETE CASCADE
 );
 -- =========================
 -- PERSON TRACKING
 -- =========================
-CREATE TABLE person_tracking (
-    id BIGSERIAL PRIMARY KEY,
-    last_name VARCHAR(100),
-    first_name VARCHAR(100),
-    dni BIGSERIAL UNIQUE,
+CREATE TABLE person_tracking
+(
+    id             BIGSERIAL PRIMARY KEY,
+    last_name      VARCHAR(100),
+    first_name     VARCHAR(100),
+    dni            BIGINT UNIQUE,
     indicator_type VARCHAR(200),
-    address VARCHAR(100),
-    phone BIGSERIAL
+    address        VARCHAR(100),
+    phone          BIGSERIAL
 );
 -- =========================
 -- EVENT
 -- =========================
-CREATE TABLE event (
-    id BIGSERIAL PRIMARY KEY,
-    name VARCHAR(100) NOT NULL,
-    type VARCHAR(50),
-    date DATE,
+CREATE TABLE event
+(
+    id          BIGSERIAL PRIMARY KEY,
+    name        VARCHAR(100) NOT NULL,
+    type        VARCHAR(50),
+    date        DATE,
     description TEXT,
-    attendees INTEGER
+    attendees   INTEGER
 );
 -- =========================
 -- EQUIPMENT TYPE
 -- =========================
-CREATE TABLE equipment_type (
-    id BIGSERIAL PRIMARY KEY,
-    name VARCHAR(100) NOT NULL,
+CREATE TABLE equipment_type
+(
+    id          BIGSERIAL PRIMARY KEY,
+    name        VARCHAR(100) NOT NULL,
     description TEXT
 );
 -- =========================
 -- EQUIPMENT
 -- =========================
-CREATE TABLE equipment (
-    id BIGSERIAL PRIMARY KEY,
-    code VARCHAR(50) NOT NULL,
-    total_stock INTEGER NOT NULL,
-    status VARCHAR(50),
-    created_at DATE,
+CREATE TABLE equipment
+(
+    id                BIGSERIAL PRIMARY KEY,
+    code              VARCHAR(50) NOT NULL,
+    total_stock       INTEGER     NOT NULL,
+    status            VARCHAR(50),
+    created_at        DATE,
     equipment_type_id BIGINT,
-    CONSTRAINT fk_equipment_type FOREIGN KEY (equipment_type_id) REFERENCES equipment_type(id)
+    CONSTRAINT fk_equipment_type FOREIGN KEY (equipment_type_id) REFERENCES equipment_type (id)
 );
 -- =========================
 -- LOAN
 -- =========================
-CREATE TABLE loan (
-                      id BIGSERIAL PRIMARY KEY,
-                      type VARCHAR(100),
-                      equipment_number VARCHAR(100),
-                      dni BIGSERIAL,
-                      applicant VARCHAR(150),
-                      address VARCHAR(200),
-                      phone BIGSERIAL,
-                      year VARCHAR(4),
-                      request_date DATE,
-                      expiration DATE,
-                      return_date DATE
+CREATE TABLE loan
+(
+    id               BIGSERIAL PRIMARY KEY,
+    type             VARCHAR(100),
+    equipment_number VARCHAR(100),
+    dni              BIGINT,
+    applicant        VARCHAR(150),
+    address          VARCHAR(200),
+    phone            BIGSERIAL,
+    year             VARCHAR(4),
+    request_date     DATE,
+    expiration       DATE,
+    return_date      DATE
+);
+
+-- =========================
+-- EVENT
+-- =========================
+CREATE TABLE users
+(
+    id         BIGSERIAL PRIMARY KEY,
+    first_name VARCHAR(100),
+    last_name  VARCHAR(100),
+    password   VARCHAR(255),
+    dni        BIGINT UNIQUE,
+    role       VARCHAR(50),
+    active     BOOLEAN
 );
--- db/data.sql
+++ db/data.sql
@@ -1,203 +1,203 @@
-INSERT INTO person (
-        first_name,
-        last_name,
-        dni,
-        civil_status,
-        date_birth,
-        tutor,
-        phone,
-        gender,
-        registration_date
-    )
-VALUES (
-        'lucas',
-        'perez',
-        30111222,
-        'soltero',
-        '1990-05-10',
-        'maria perez',
-        3884011111,
-        'masculino',
-        '2023-01-10'
-    ),
-    (
-        'ramon',
-        'fernandez',
-        12111278,
-        'casado',
-        '1993-05-10',
-        'gaston perez',
-        3884011111,
-        'masculino',
-        '2024-01-10'
-    ),
-    (
-        'juan',
-        'gomez',
-        45114652,
-        'soltero',
-        '2000-05-12',
-        'jorge perez',
-        3884011111,
-        'masculino',
-        '2025-01-10'
-    );
-INSERT INTO education (name, address, education_level, person_id)
-VALUES (
-        'escuela belgrano',
-        'address',
-        'primario',
-        1
-    ),
-    (
-        'raul galan',
-        'calle falsa',
-        'primario',
-        2
-    ),
-    (
-        'comercio 4',
-        'av. pte. peron',
-        'secundario',
-        3
-    );
-INSERT INTO work (
-        company_name,
-        status,
-        address,
-        social_work,
-        name_social_work,
-        person_id
-    )
-VALUES (
-        'empresa ledesma',
-        'activo',
-        'av siempre viva 123',
-        true,
-        'osde',
-        1
-    ),
-    (
-        'muni ledesma',
-        'activo',
-        'av siempre viva 123',
-        true,
-        'isj',
-        2
-    ),
-    (
-        'ejesa',
-        'activo',
-        'av libertad',
-        true,
-        'ospail',
-        3
-    );
-INSERT INTO health (
-        active_cud,
-        rehabilitation_treatment,
-        cud_number,
-        diagnostic,
-        disability_type,
-        person_id
-    )
-VALUES (true, true, 'cud1001', 'motor', 'fisica', 1),
-    (true, true, 'cud1001', 'motor', 'fisica', 2),
-    (true, true, 'cud1001', 'motor', 'fisica', 3);
-INSERT INTO address (street, district, locality, province, person_id)
-VALUES (
-        'calle 1',
-        'barrio centro',
-        'libertador',
-        'jujuy',
-        1
-    ),
-    (
-        'calle 2',
-        'barrio centro',
-        'libertador',
-        'jujuy',
-        2
-    ),
-    (
-        'calle 3',
-        'barrio centro',
-        'libertador',
-        'jujuy',
-        3
-    );
-INSERT INTO benefit (
-        pension,
-        free_pass,
-        federal_program,
-        auh,
-        merchandise,
-        person_id
-    )
-VALUES (true, true, false, false, true, 1),
-    (true, true, false, false, true, 2),
-    (true, true, false, false, true, 3);
-INSERT INTO family_member (
-        first_name,
-        last_name,
-        dni,
-        age,
-        civil_status,
-        parentage,
-        occupation,
-        person_id
-    )
-VALUES (
-        'maria',
-        'perez',
-        40111111,
-        60,
-        'casado',
-        'madre',
-        'ama de casa',
-        1
-    ),
-    (
-        'dante',
-        'gomez',
-        12345678,
-        60,
-        'casado',
-        'padre',
-        'empleado',
-        2
-    ),
-    (
-        'lucas',
-        'fernandez',
-        40111111,
-        60,
-        'casado',
-        'primo',
-        'estudiante',
-        3
-    );
-INSERT INTO event (name, type, date, description, attendees)
-VALUES (
-        'Reunión de equipo',
-        'Taller',
-        '2026-03-15',
-        'Reunión para planificación del proyecto',
-        10
-    );
-INSERT INTO event (name, type, date, description, attendees)
-VALUES (
-        'Conferencia de Tecnología',
-        'Charla',
-        '2026-04-02',
-        'Evento sobre desarrollo de software',
-        120
-    );
-INSERT INTO event (name, type, date, description, attendees)
-VALUES (
-        'Taller de Programación',
-        'Taller',
-        '2026-04-10',
-        'Taller práctico de Java y Spring Boot',
-        25
-    );
+-- INSERT INTO person (
+--         first_name,
+--         last_name,
+--         dni,
+--         civil_status,
+--         date_birth,
+--         tutor,
+--         phone,
+--         gender,
+--         registration_date
+--     )
+-- VALUES (
+--         'lucas',
+--         'perez',
+--         30111222,
+--         'soltero',
+--         '1990-05-10',
+--         'maria perez',
+--         3884011111,
+--         'masculino',
+--         '2023-01-10'
+--     ),
+--     (
+--         'ramon',
+--         'fernandez',
+--         12111278,
+--         'casado',
+--         '1993-05-10',
+--         'gaston perez',
+--         3884011111,
+--         'masculino',
+--         '2024-01-10'
+--     ),
+--     (
+--         'juan',
+--         'gomez',
+--         45114652,
+--         'soltero',
+--         '2000-05-12',
+--         'jorge perez',
+--         3884011111,
+--         'masculino',
+--         '2025-01-10'
+--     );
+-- INSERT INTO education (name, address, education_level, person_id)
+-- VALUES (
+--         'escuela belgrano',
+--         'address',
+--         'primario',
+--         1
+--     ),
+--     (
+--         'raul galan',
+--         'calle falsa',
+--         'primario',
+--         2
+--     ),
+--     (
+--         'comercio 4',
+--         'av. pte. peron',
+--         'secundario',
+--         3
+--     );
+-- INSERT INTO work (
+--         company_name,
+--         status,
+--         address,
+--         social_work,
+--         name_social_work,
+--         person_id
+--     )
+-- VALUES (
+--         'empresa ledesma',
+--         'activo',
+--         'av siempre viva 123',
+--         true,
+--         'osde',
+--         1
+--     ),
+--     (
+--         'muni ledesma',
+--         'activo',
+--         'av siempre viva 123',
+--         true,
+--         'isj',
+--         2
+--     ),
+--     (
+--         'ejesa',
+--         'activo',
+--         'av libertad',
+--         true,
+--         'ospail',
+--         3
+--     );
+-- INSERT INTO health (
+--         active_cud,
+--         rehabilitation_treatment,
+--         cud_number,
+--         diagnostic,
+--         disability_type,
+--         person_id
+--     )
+-- VALUES (true, true, 'cud1001', 'motor', 'fisica', 1),
+--     (true, true, 'cud1001', 'motor', 'fisica', 2),
+--     (true, true, 'cud1001', 'motor', 'fisica', 3);
+-- INSERT INTO address (street, district, locality, province, person_id)
+-- VALUES (
+--         'calle 1',
+--         'barrio centro',
+--         'libertador',
+--         'jujuy',
+--         1
+--     ),
+--     (
+--         'calle 2',
+--         'barrio centro',
+--         'libertador',
+--         'jujuy',
+--         2
+--     ),
+--     (
+--         'calle 3',
+--         'barrio centro',
+--         'libertador',
+--         'jujuy',
+--         3
+--     );
+-- INSERT INTO benefit (
+--         pension,
+--         free_pass,
+--         federal_program,
+--         auh,
+--         merchandise,
+--         person_id
+--     )
+-- VALUES (true, true, false, false, true, 1),
+--     (true, true, false, false, true, 2),
+--     (true, true, false, false, true, 3);
+-- INSERT INTO family_member (
+--         first_name,
+--         last_name,
+--         dni,
+--         age,
+--         civil_status,
+--         parentage,
+--         occupation,
+--         person_id
+--     )
+-- VALUES (
+--         'maria',
+--         'perez',
+--         40111111,
+--         60,
+--         'casado',
+--         'madre',
+--         'ama de casa',
+--         1
+--     ),
+--     (
+--         'dante',
+--         'gomez',
+--         12345678,
+--         60,
+--         'casado',
+--         'padre',
+--         'empleado',
+--         2
+--     ),
+--     (
+--         'lucas',
+--         'fernandez',
+--         40111111,
+--         60,
+--         'casado',
+--         'primo',
+--         'estudiante',
+--         3
+--     );
+-- INSERT INTO event (name, type, date, description, attendees)
+-- VALUES (
+--         'Reunión de equipo',
+--         'Taller',
+--         '2026-03-15',
+--         'Reunión para planificación del proyecto',
+--         10
+--     );
+-- INSERT INTO event (name, type, date, description, attendees)
+-- VALUES (
+--         'Conferencia de Tecnología',
+--         'Charla',
+--         '2026-04-02',
+--         'Evento sobre desarrollo de software',
+--         120
+--     );
+-- INSERT INTO event (name, type, date, description, attendees)
+-- VALUES (
+--         'Taller de Programación',
+--         'Taller',
+--         '2026-04-10',
+--         'Taller práctico de Java y Spring Boot',
+--         25
+--     );
